ccd44ee5a23a5521ac4c9436b1e153b17e67b806
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-10-24 16:02:47 +0000

    タブブラウザ拡張の代替としてのメタパッケージ
    git-svn-id: https://www.cozmixng.org/repos/piro/tabextensions3@1324 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ tabextensions3.bat
@@ -0,0 +1,40 @@
+setlocal
+set appname=%~n0
+
+d:
+cd d:\data\codes\%appname%
+
+
+del "*.xpi"
+
+mkdir xpi_temp
+copy *.rdf xpi_temp\
+cd xpi_temp
+
+copy o:\xul\xpi\multipletab.xpi .\
+copy o:\xul\xpi\informationaltab.xpi .\
+copy o:\xul\xpi\treestyletab.xpi .\
+copy o:\xul\xpi\viewsourceintab.xpi .\
+
+chmod -cfr 644 *.rdf *.xpi
+zip -9 "..\%appname%.xpi" *.rdf *.xpi
+del "o:\xul\xpi\%appname%.xpi"
+mv ..\%appname%.xpi o:\xul\xpi\
+
+
+
+del "*.xpi"
+
+copy d:\data\codes\multipletab\multipletab_noupdate.xpi .\
+copy d:\data\codes\informationaltab\informationaltab_noupdate.xpi .\
+copy d:\data\codes\treestyletab\treestyletab_noupdate.xpi .\
+copy d:\data\codes\viewsourceintab\viewsourceintab_noupdate.xpi .\
+
+chmod -cfr 644 *.rdf *.xpi
+zip -9 "..\%appname%_noupdate.xpi" *.rdf *.xpi
+
+cd ..
+rm -r xpi_temp
+
+
+endlocal
